b470e1c4668a673abb1a0ac59ebc2e63598b54e2
pk <pk@PK2-00>
2022-11-05 16:45:18 +0100

    Dodaj pliki projektów.

---
+++ tablice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Tablice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tab = new double[1000];
+            int i = 0;
+            string linia;
+          
+            do
+            {
+                Console.WriteLine("Podaj liczbę nr {0}. Enter kończy wczytywanie.", i);
+                linia = Console.ReadLine();
+                if (linia.Length == 0) break;
+                tab[i++] = Convert.ToDouble(linia);
+            } while (i < tab.Length );
+
+            Console.WriteLine("Podano tyle liczb: {0}", i);
+            Console.WriteLine("A teraz w odwrotnej kolejności");
+
+            for (int k = i-1; k >=0 ; k--)
+            {
+                Console.WriteLine("Element {0} wynosi {1}", k, tab[k]);
+            }
+        }
+    }
+}
